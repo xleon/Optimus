adf2ad57a1cef981c15d3223e16f4d167c435396
Diego <malandro@gmail.com>
2020-05-03 03:01:35 +0200

    Fix tests

--- Optimus.Tests/NumberExtensionsTests.cs
+++ Optimus.Tests/NumberExtensionsTests.cs
@@ -8,11 +8,11 @@ namespace Optimus.Tests
     public class NumberExtensionsTests
     {
         [Test]
-        [TestCase(1024, "1Kb")]
-        [TestCase(2048, "2Kb")]
-        [TestCase(2100, "2Kb")]
-        [TestCase(700, "700B")]
-        [TestCase(1073741824, "1Gb")]
+        [TestCase(1024, "1 KB")]
+        [TestCase(2048, "2 KB")]
+        [TestCase(2100, "2 KB")]
+        [TestCase(700, "700 B")]
+        [TestCase(1073741824, "1 GB")]
         public void ToFileLengthRepresentation(long amount, string expected)
         {
             amount.ToFileLengthRepresentation().ShouldBe(expected);
